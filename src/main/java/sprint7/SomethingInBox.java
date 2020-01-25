007d56e680aadd7a9baf8bea9c83b4795bb33f97
urithegreat <bektenov.urmat@gmail.com>
2020-01-24 20:22:16 -0600

    Fixed the bug

--- src/main/java/sprint7/SomethingInBox.java
+++ src/main/java/sprint7/SomethingInBox.java
@@ -1,6 +1,7 @@
 package sprint7;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SomethingInBox {
@@ -13,12 +14,29 @@ public class SomethingInBox {
 
     public static void getInput(){
         System.out.println("Please enter the length of your array:");
-        int arrayLength = scanner.nextInt();
+        int arrayLength = 0;
+        try{
+            arrayLength = scanner.nextInt();
+            if(arrayLength<1){
+                System.err.println("The length of the array must be defined with a whole number greater than 1!");
+                System.exit(-1);
+            }
+        }
+        catch (InputMismatchException e){
+            System.err.println("The length of the array must be defined with a whole number greater than 1!");
+            System.exit(-1);
+        }
         strings = new String[arrayLength];
         int counter = 0;
         while(counter<strings.length){
             System.out.println("Please enter your element:");
             strings[counter] = scanner.next();
+            for(int i = 0; i<strings[counter].length(); i++){
+                if(strings[counter].charAt(i)!='#' && strings[counter].charAt(i)!='*'){
+                    System.out.println("Please enter a valid string, only # and * allowed!");
+                    System.exit(-1);
+                }
+            }
             counter++;
         }
     }
@@ -32,6 +50,9 @@ public class SomethingInBox {
                 for(int j = indexOfAsterisk; j<strings[i].length()-1; j++) {
                     if (strings[i].charAt(0) == '#' && strings[i].charAt(indexOfAsterisk + 1) == '#') {
                         isPresent = true;
+                    }
+                    else{
+                        return false;
                     }
                 }
             }
